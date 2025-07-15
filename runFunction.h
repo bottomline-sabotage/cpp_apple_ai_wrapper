748a70a7c70644f80e4e6c7f833e9f33cc2dbb0e
Tyler Koberna <kobernajamestyler+github@gmail.com>
2025-07-15 00:23:08 -0600

    Update runFunction.h
    removed old instructions which no longer apply

--- runFunction.h
+++ runFunction.h
@@ -1,7 +1,5 @@
 /** Is used by Swift in order to run C++ functions from within Swift (used for cbs) 
  * 
- * @warning I don't know why, but this **must be compiled to run_function.dylib and runFunction.dylib**. I don't know why, but Swift's linker complains otherwise. 
- * 
 */
 
 #include <iostream>
